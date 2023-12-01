14a197bdc1da3d909e23533611132e1db700b746
Rutvij Dave <rutvijdave@Rutvijs-MacBook-Pro.local>
2023-12-01 18:49:17 -0500

    Refund Payment PM2 config added

--- pm2.config.js
+++ pm2.config.js
@@ -4,10 +4,13 @@ module.exports = {
         name: 'Refund-Payment-api',
         script: 'index.js',  // Replace with your actual main script file
         instances: 'max',
-        exec_mode: 'cluster',
-        watch: true,
         ignore_watch: ['node_modules', 'logs'],
         max_memory_restart: '1G',
+        instances: 1,
+        exec_mode: 'fork',
+        cron_restart: "*/10 * * * *",
+        watch: false,
+        autorestart: false
       },
     ],
   };
